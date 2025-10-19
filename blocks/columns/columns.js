be7b49c642d92457b5ef2d933dc957c2f9c372b7
Bharath <bharath.b1@network.lilly.com>
2025-10-19 13:48:42 +0530

    placeholder

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,6 +1,6 @@
-import { fetchPlaceholders } from "../../scripts/placeholders";
+import { fetchPlaceholders } from '../../scripts/placeholders.js';
 
-export default async function decorate(block) {
+export default async function decorate() {
   const placeholders = await fetchPlaceholders('en');
   console.log('Placeholders:', placeholders);
 }
